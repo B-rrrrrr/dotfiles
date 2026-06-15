6438200068d3e9d9e80ea87ca72a99f251290c16
B-rrrrrr <rayahunter15@gmail.com>
2026-06-15 08:55:48 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,41 +1,33 @@
-#for vm: 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
-#for acer:
-#for thinkpad:
-
 sudo pacman -S --needed \
-	networkmanager \
-	hyprland \
-	waybar \
-	wofi \
-	kitty \
-	git \
-	pipewire \
-	pipewire-pulse \
-	wireplumber \
-	pavucontrol \
-	fastfetch \
-	python-pywal \
-	firefox \
-	grim slurp \
-	wl-clipboard \
-	mako
+  networkmanager \
+  hyprland \
+  waybar \
+  wofi \
+  kitty \
+  git \
+  pipewire \
+  pipewire-pulse \
+  wireplumber \
+  pavucontrol \
+  fastfetch \
+  python-pywal \
+  firefox \
+  grim slurp \
+  wl-clipboard \
+  mako
 
-systemctl enable --now NetworkManager
-systemctl enable --now pipewire pipewire-pulse wireplumber
+sudo systemctl enable --now NetworkManager
 
-mkdir -p ~/.config
+systemctl --user enable --now pipewire pipewire-pulse wireplumber
+
+mkdir -p ~/.config ~/.local/bin ~/Pictures/Wallpapers
 
 ln -sf ~/dotfiles/hypr ~/.config/hypr
 ln -sf ~/dotfiles/waybar ~/.config/waybar
 ln -sf ~/dotfiles/wofi ~/.config/wofi
 ln -sf ~/dotfiles/kitty ~/.config/kitty
 
-mkdir -p ~/Pictures/Wallpapers
-
-ln -sf ~/dotfiles/Wallpapers/* ~/Pictures/Wallpapers
-
-mkdir -p ~/.local/bin
+cp -r ~/dotfiles/Wallpapers/* ~/Pictures/Wallpapers/
 
 ln -sf ~/dotfiles/bin/wallpaper.sh ~/.local/bin/wallpaper.sh
-
-
+chmod +x ~/.local/bin/wallpaper.sh
