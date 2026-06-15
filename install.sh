6631145d88764acd53dac932af63802e0db06eb7
B-rrrrrr <rayahunter15@gmail.com>
2026-06-15 08:58:56 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,16 +18,18 @@ sudo pacman -S --needed \
 
 sudo systemctl enable --now NetworkManager
 
-systemctl --user enable --now pipewire pipewire-pulse wireplumber
+systemctl --user enable pipewire pipewire-pulse wireplumber
 
 mkdir -p ~/.config ~/.local/bin ~/Pictures/Wallpapers
+
+rm -rf ~/.config/hypr ~/.config/waybar ~/.config/wofi ~/.config/kitty
 
 ln -sf ~/dotfiles/hypr ~/.config/hypr
 ln -sf ~/dotfiles/waybar ~/.config/waybar
 ln -sf ~/dotfiles/wofi ~/.config/wofi
 ln -sf ~/dotfiles/kitty ~/.config/kitty
 
-cp -r ~/dotfiles/Wallpapers/* ~/Pictures/Wallpapers/
+ln -sf ~/dotfiles/Wallpapers ~/Pictures/Wallpapers
 
 ln -sf ~/dotfiles/bin/wallpaper.sh ~/.local/bin/wallpaper.sh
 chmod +x ~/.local/bin/wallpaper.sh
