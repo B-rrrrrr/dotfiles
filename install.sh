236eda13c98693804433eb5814ac03b320122830
B-rrrrrr <rayahunter15@gmail.com>
2026-06-15 09:19:00 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -29,7 +29,7 @@ ln -sf ~/dotfiles/waybar ~/.config/waybar
 ln -sf ~/dotfiles/wofi ~/.config/wofi
 ln -sf ~/dotfiles/kitty ~/.config/kitty
 
-ln -sf ~/dotfiles/Wallpapers/ ~/Pictures/Wallpapers
+ln -sf ~/dotfiles/Wallpapers/ ~/Pictures
 
 ln -sf ~/dotfiles/bin/wallpaper ~/.local/bin/wallpaper
 chmod +x ~/.local/bin/wallpaper
